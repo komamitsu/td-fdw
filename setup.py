607dc9cf1dcd9e133028782faf88ccb97c4eb69f
Mitsunori Komatsu <komamitsu@gmail.com>
2015-07-06 13:33:19 +0900

    Change 'name' in setup.py to 'td-fdw'

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='TreasureDataFdw',
+setup(name='td-fdw',
       version='0.0.1',
       description='A foreign data wrapper for TreasureData',
       author='Mitsunori Komatsu',
