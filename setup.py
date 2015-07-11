cda530d95ba683489969fc5ae8ea2e1b0cfe13bb
Mitsunori Komatsu <komamitsu@gmail.com>
2015-07-11 23:49:58 +0900

    Update version

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
 setup(name='td-fdw',
-      version='0.0.1',
+      version='0.0.2',
       description='A foreign data wrapper for TreasureData',
       author='Mitsunori Komatsu',
       author_email='komamitsu@gmail.com',
+      license = "Apache",
       url='https://github.com/komamitsu/td-fdw',
       install_requires=['td-client', 'certifi'],
       packages=['tdfdw']
